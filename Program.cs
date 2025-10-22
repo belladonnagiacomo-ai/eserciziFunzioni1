f4e1f54c4b61b9900c21c4114236af9c077b9217
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2025-10-22 16:36:45 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace eserciziFunzioni
+{
+    internal class Program
+    {
+        static bool verificaN(int n)
+        {
+
+            int r = n % 5;
+            if (r == 0)
+            {
+                return true;
+            }
+
+            return false;
+
+
+        }
+        static int sommaN()
+        {
+            int s = 0;
+            for (int i = 1; i <= 50; i++)
+            {
+                if (verificaN(i) == true)
+                {
+                    s = s + i;
+                }
+
+            }
+            return s;
+        }
+        static string versioneS(int n)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            int ris = sommaN();
+            Console.WriteLine(ris);
+
+        }
+        fdkjshgytgfvdasfwedabs
+    }
+}
