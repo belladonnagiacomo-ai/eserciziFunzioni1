153ee175f2559a5d130be3d25f2237ee8250ea95
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2025-10-22 17:12:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -30,26 +30,55 @@ namespace eserciziFunzioni
             }
             return s;
         }
-        static bool controlloA(bool n)
+        static bool controlloA (string  n)
         {
             string a = "Marco", b = "Giacomo", c = "Luca", d = "Alessio";
             if (n = a || b || c || d)
             {
-
-
-
+                return true;
             }
 
-           return n;
+           return false;
         }
+        static bool passwordS(int p)
+        {
+            p = 1234;
+            if (p == 1234)
+            {
+                return true;
+            }
 
+            return false;
+        }
         static void Main(string[] args)
         {
             int ris = sommaN();
             Console.WriteLine(ris);
             Console.WriteLine("dammi un nome:");
-            bool nome = Convert.ToBoolean(Console.ReadLine());
+            string nome = Console.ReadLine();
             controlloA(nome);
+            if (controlloA(nome) == true) {
+
+                int p = 0;
+                while (passwordS(p) == false)
+                {
+                    Console.WriteLine("dimmi la password per entrare(sono 4 numeri):");
+                    p = Convert.ToInt32(Console.ReadLine());
+                    passwordS(p);
+
+                    if (passwordS(p) == true)
+                    {
+                        Console.WriteLine("sei entrato");
+                    }
+                    else
+                    {
+                        Console.WriteLine("non sei entrato, riprova");
+                    }
+                }
+
+            
+            }
+
 
         }
         
